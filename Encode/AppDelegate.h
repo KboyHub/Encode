c03993fbb2120d709161595e72a2d90244a65ad6
KboyHub <yankang_hpu@163.com>
2017-03-12 12:01:21 +0800

    Initial Commit

---
+++ Encode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encode
+//
+//  Created by yankang on 2017/3/12.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Encode
+//
+//  Created by yankang on 2017/3/12.
+//  Copyright © 2017年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
